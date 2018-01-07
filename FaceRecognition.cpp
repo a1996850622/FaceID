7995d37c819d5d32784ce79f66764f340255c556
a1996850622 <a1996850622@gmail.com>
2018-01-07 22:26:06 +0800

    Update

--- FaceRecognition.cpp
+++ FaceRecognition.cpp
@@ -19,7 +19,6 @@ CascadeClassifier face_cascade;
 
 int main(void){
 	Mat frame;
-	Mat edges;
 	Mat frame_gray;
 	Mat face;
 	Mat face_test;
@@ -27,26 +26,35 @@ int main(void){
 	vector<Rect> faces(0);
 	bool stop = false;
 
+	/* Open the webcamera */
 	VideoCapture capture(0);
 
+	/* Check whether the camera is opened */
 	if(!capture.isOpened())
 		PANIC("Error open camera!");
 
+	/* Loading face classifier */
 	if(!face_cascade.load(face_cascade_name))
 		PANIC("Error loading face cascade!");
 
+	/* Create EigenFaceRecognizer and load our PCA model */
 	Ptr<EigenFaceRecognizer> model = EigenFaceRecognizer::create();
 	model->read("Source/MyFacePCAModel.xml");
 
 	while(!stop){
+		/* Get the frame from camera */
 		capture >> frame;
 
+		/* Change the frame to gray scale */
 		cvtColor(frame, frame_gray, CV_BGR2GRAY);
 
+		/* Histogram equalization */
 		equalizeHist(frame_gray, frame_gray);
 
+		/* Detect faces of different sizes using cascade classifier */
 		face_cascade.detectMultiScale(frame_gray, faces, 1.05, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
 
+		/* Draw circles around the faces */
 		for(size_t i=0; i<faces.size(); i++){
 			if(faces[i].height>0 && faces[i].width>0){
 				face = frame_gray(faces[i]);
@@ -57,9 +65,11 @@ int main(void){
 		}
 		
 		int predictPCA = 0;
+
 		if(face.rows >= 120)
 			resize(face, face_test, Size(92, 112));
 
+		/* Predict which data set face_test is in */
 		if(!face_test.empty())
 			predictPCA = model->predict(face_test);
 
@@ -70,6 +80,7 @@ int main(void){
 		}
 
 		imshow("face", frame);
+
 		if(waitKey(50) >= 0)
 			stop = true;
 	}
