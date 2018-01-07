d6bf0f17ed2c239a32585a3b2a5d0dd1c5180516
undecV <undecv@gmail.com>
2018-01-08 01:02:18 +0800

    make csv

--- CSV.py
+++ CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-import sys
 import os.path
 
 # This is a tiny script to help you creating a CSV file from a face
@@ -24,29 +24,22 @@ import os.path
 #  |   |-- 10.pgm
 #
 
+script_path = os.path.abspath(os.path.dirname(__file__))
+
 if __name__ == "__main__":
+    BASE_PATH = os.path.join(script_path, "att_faces")
+    SEPARATOR = ";"
 
-    #if len(sys.argv) != 2:
-    #    print "usage: create_csv <base_path>"
-    #    sys.exit(1)
-
-    #BASE_PATH=sys.argv[1]
-    BASE_PATH="/home/chris/Github/FaceID/att_faces"
-    
-    SEPARATOR=";"
-
-    fh = open("att_faces/at.txt",'w')
-
-    label = 0
-    for dirname, dirnames, filenames in os.walk(BASE_PATH):
-        for subdirname in dirnames:
-            subject_path = os.path.join(dirname, subdirname)
-            for filename in os.listdir(subject_path):
-                abs_path = "%s/%s" % (subject_path, filename)
-                #print "%s%s%d" % (abs_path, SEPARATOR, label)
-                fh.write(abs_path)
-                fh.write(SEPARATOR)
-                fh.write(str(label))
-                fh.write("\n")      
-            label = label + 1
-    fh.close()
+    with open("att_faces/at.txt", 'w') as fh:
+        label = 0
+        for dirname, dirnames, filenames in os.walk(BASE_PATH):
+            for subdirname in dirnames:
+                subject_path = os.path.join(dirname, subdirname)
+                for filename in os.listdir(subject_path):
+                    abs_path = "%s%s%s" % (subject_path, os.path.sep, filename)
+                    # print "%s%s%d" % (abs_path, SEPARATOR, label)
+                    fh.write(abs_path)
+                    fh.write(SEPARATOR)
+                    fh.write(str(label))
+                    fh.write("\n")
+                label = label + 1
